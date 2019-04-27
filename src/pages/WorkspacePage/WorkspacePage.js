961d046c45a9cca55403ce7dcb1c71ced61ed02a
me <49545028+parismaris@users.noreply.github.com>
2019-04-27 11:37:48 -0400

    feat-taskBoxAdder

--- src/pages/WorkspacePage/WorkspacePage.js
+++ src/pages/WorkspacePage/WorkspacePage.js
@@ -2,7 +2,8 @@
 
 import React from 'react'
 import ViewBox from '../../comp/ViewBox/ViewBox'
-import TaskBox from '../../comp/TaskBox/TaskBox'
+import TaskBoxAdder from '../../comp/TaskBoxAdder/TaskBoxAdder'
+// import TaskBox from '../../comp/TaskBox/TaskBox'
 import D from '../../comp/D/D'
 
 const WorspacePage = () => (
@@ -17,26 +18,14 @@ const WorspacePage = () => (
       [alias="page-cont-wordList"] [comp="S"],
       _ { padding-left:3px; padding-right:3px; background:white; color:green; }
 
+      [alias="taskBoxAdder"] { margin-left:30px; margin-top:120px; margin-bottom:30px; min-width:360px; width:360px; }
       [alias="taskBox"] { margin-left:30px; margin-top:120px; margin-bottom:30px; min-width:360px; width:360px; }
       [alias="viewboxMain"] { margin-right:30px; }
     `}</style>
 
     <ViewBox alias="viewboxMain" s={{overflowX:"scroll", overflowY:"hidden", display:"flex", background:'skyblue'}}>
 
-      <TaskBox alias="taskBox">
-        <D alias="page-cont-wordList">
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-        </D>
-      </TaskBox>
+      <TaskBoxAdder alias="taskBoxAdder" />
 
       <D alias="spacer" s={{display:'inline-block', position:'relative', minWidth:'30px'}}></D>
     </ViewBox>
