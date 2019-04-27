ca6732c7e3de0af155c88c0e9f161fb87a287a51
me <49545028+parismaris@users.noreply.github.com>
2019-04-27 10:59:54 -0400

    ref-taskBoxFromViewBox

--- src/pages/WorkspacePage/WorkspacePage.js
+++ src/pages/WorkspacePage/WorkspacePage.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import ViewBox from '../../comp/ViewBox/ViewBox'
+import TaskBox from '../../comp/TaskBox/TaskBox'
 import D from '../../comp/D/D'
 
 const WorspacePage = () => (
@@ -16,13 +17,13 @@ const WorspacePage = () => (
       [alias="page-cont-wordList"] [comp="S"],
       _ { padding-left:3px; padding-right:3px; background:white; color:green; }
 
-      [alias="viewbox"] { margin-left:30px; margin-top:120px; margin-bottom:30px; min-width:360px; width:360px; }
+      [alias="taskBox"] { margin-left:30px; margin-top:120px; margin-bottom:30px; min-width:360px; width:360px; }
       [alias="viewboxMain"] { margin-right:30px; }
     `}</style>
 
     <ViewBox alias="viewboxMain" s={{overflowX:"scroll", overflowY:"hidden", display:"flex", background:'skyblue'}}>
 
-      <ViewBox alias="viewbox">
+      <TaskBox alias="taskBox">
         <D alias="page-cont-wordList">
           <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
           <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
@@ -35,34 +36,10 @@ const WorspacePage = () => (
           <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
           <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
         </D>
-      </ViewBox>
-
-      <ViewBox alias="viewbox">
-        <D alias="page-cont-wordList">
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-        </D>
-      </ViewBox>
-
-      <ViewBox alias="viewbox">
-        <D alias="page-cont-wordList">
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-          <D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D><D>DDDD</D>
-        </D>
-      </ViewBox>
+      </TaskBox>
 
       <D alias="spacer" s={{display:'inline-block', position:'relative', minWidth:'30px'}}></D>
     </ViewBox>
-
 
   </>
 )
