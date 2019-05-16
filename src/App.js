e33086036aee0db4047d8f1ae4cb192f30795a85
me <49545028+parismaris@users.noreply.github.com>
2019-05-15 22:09:05 -0400

    feat-which

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { hot } from 'react-hot-loader'
-// import Viewport from './comp/Viewport/Viewport'
+import { which } from './lib/global/global'
 import WorkspacePage from './pages/WorkspacePage/WorkspacePage'
 import AppHeader from './comp/AppHeader/AppHeader'
 import Counter from './comp/Counter/Counter'
@@ -23,7 +23,29 @@ const compName = 'App'
 
 const App = (props: Props) => {
   //document.body.classList.add('u-panes');
-  
+
+  which(2, {
+    1: () => console.log('ONE'),
+    2: () => console.log('TWO'),
+    3: () => console.log('THREE'),    
+  }, 'DEFAULT_VALUE');
+
+  which(42, {
+    1: 'someString',
+    2: {some: 'object'},
+    3: () => console.log('someExecutedFunction'),    
+  }, () => {
+    console.log('VALUE NOT FOUND')
+  });
+
+  const state = { count: 50 }
+  const action = { type:'INCREMENT_COUNTER', value: 1 }
+  const newState =  which(action.type, {
+    'INCREMENT_COUNTER': { ...state, ...{count: state.count + action.value} },
+    'DECREMENT_COUNTER': { ...state, ...{count: state.count + action.value} },
+  }, state);
+  console.log('NEWSTATE:', newState);
+
   return (
     <div data-app-container>
       <AppHeader/>
