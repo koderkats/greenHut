570642095e9beaa77bfde8296d2bdc70ae15cf56
me <49545028+parismaris@users.noreply.github.com>
2019-05-16 22:13:25 -0400

    feat-debugging

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import React, {useContext} from 'react'
 import { hot } from 'react-hot-loader'
 import {  StoreProvider, /*StoreContext*/ } from './context/StoreContext'
 
-import { which } from './lib/global/global'
+import { which, func, log, dir } from './lib/global/global'
 import WorkspacePage from './pages/WorkspacePage/WorkspacePage'
 import AppHeader from './comp/AppHeader/AppHeader'
 import Counter from './comp/Counter/Counter'
 import TaskList from './comp/TaskList/TaskList'
 import s from './App.css'
 
+const thisFile = 'App.js'
 // const { state, dispatch, actions } = useContext(StoreContext);
 
 type Props = {
@@ -26,20 +27,24 @@ function Divider() {
 const compName = 'App'
 
 const App = (props: Props) => {
+  const thisFunc = 'App'
+  func(thisFile, thisFunc, props);
+
   //document.body.classList.add('u-panes');
 
+
   which(2, {
-    1: () => console.log('ONE'),
-    2: () => console.log('TWO'),
-    3: () => console.log('THREE'),    
+    1: () => log(thisFile, thisFunc, 'ONE'),
+    2: () => log(thisFile, thisFunc, 'TWO'),
+    3: () => log(thisFile, thisFunc, 'THREE'),    
   }, 'DEFAULT_VALUE');
 
   which(42, {
     1: 'someString',
     2: {some: 'object'},
-    3: () => console.log('someExecutedFunction'),    
+    3: () => log(thisFile, thisFunc, 'someExecutedFunction'),    
   }, () => {
-    console.log('VALUE NOT FOUND')
+    log(thisFile, thisFunc, 'VALUE NOT FOUND')
   });
 
   const curState = { count: 50 }
--- src/lib/global/global.js
+++ src/lib/global/global.js
@@ -1,3 +1,28 @@
+export const isDev = true
+export const isDebug = isDev
+const logFunc = true
+const logLog = true
+const logDir = true
+
+export function func(file, func, msg) {
+  if (isDebug && logFunc){
+		console.log('[func]', file, func, msg ? msg : '');
+	}
+}
+
+export function log(file, func, msg) {
+  if (isDebug && logLog){
+		console.log('[log]', file, func, msg);
+	}
+}
+
+export function dir(file, func, obj, msg) {
+  if (isDebug && logDir){
+		console.log('[dir]', file, func, msg ? msg + ' DIR:' : 'DIR:');
+		console.dir(obj);
+	}
+}
+
 export function hello() {
 	console.log('world');
 }
@@ -10,3 +35,4 @@ export function which(key, obj, def) {
 	if (typeof def !== 'undefined') return isFunction(def) ? def() : def;
 	return null;
 }
+
